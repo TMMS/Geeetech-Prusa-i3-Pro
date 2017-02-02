8f0cb9e6b12917a77b176729d747e50903507be2
Tiago Silva <tiago.miguel.martins.silva@gmail.com>
2017-02-02 16:50:24 +0000

    Added the Geeetech Prusa i3 Aluminum settings

---
+++ Marlin/Load_Aluminum_Settings.bat
@@ -0,0 +1,8 @@
+@echo off
+echo *********************************************
+echo Loading Geeetech Prusa i3 Aluminum settings...
+echo *********************************************
+echo. 
+copy .\example_configurations\Geeetech_Prusa_i3_Aluminum\*.* /Y
+echo. 
+echo Done!
